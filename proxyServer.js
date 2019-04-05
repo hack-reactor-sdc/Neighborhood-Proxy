b8527f77766ec23f8e0f3455c3fdaf3db293a7f3
Andrew Schuessler <ajschuessler@gmail.com>
2019-04-05 10:17:51 -0400

    Installed nodemon and set up script to run nodemon on proxy server.

--- proxyServer.js
+++ proxyServer.js
@@ -3,9 +3,27 @@ const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+const axios = require('axios');
+const cors = require('cors');
+var bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/items', (req, res) => {
+    axios.get('http://localhost:3007/items')
+    .then(result => res.send(result.data))
+    .catch(err => console.log(err))
+})
+
+app.get('/items/:id', (req, res) => {
+    var id = req.params.id;
+    axios.get(`http://localhost:3007/items/${id}`)
+    .then(result => res.send(result.data))
+    .catch(err => console.log(err))
+})
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
